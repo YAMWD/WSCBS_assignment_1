2d833d98f3cb34d5c2336bf0f1f67d661f176fbb
Zhiyan Yao <zhiyan@sp-byods-145-109-17-116.wireless.uva.nl>
2024-02-12 13:57:33 +0100

    url validation and hashing

--- main.py
+++ main.py
@@ -44,20 +44,27 @@ def get_identifiers():
 # create a new identifier for the url
 @app.route("/<url>", methods=["POST"])
 def create_identifier(url):
-    try:
-        # Check URL validity with a regex expression before creating a mapping for it
-        # if check_url_validity(url):
-        #     identifier = hashlib.sha256(url.encode()).hexdigest()
-        #     urls[identifier] = url
-        #     return 201, identifier
-        # else:
-        #     return 404, "Invalid URL"
-        identifier = hashlib.sha256(url.encode()).hexdigest()
-        urls[identifier] = url
-        return identifier, 201
-    except:
-        return "Create Failed", 404
+
+    # Check URL validity with a regex expression before creating a mapping for it
+    if check_url_validity(url) == False:
+        return  "Invalid URL", 400
+
+    if url in urls.values():
+        id = [k for k, v in urls.items() if v == url][0]
+        return 'identifier of {} already exists'.format(url), 400
+
+    identifier = '-1'
+    for i in range(len(urls)):
+        if str(i) not in urls.keys():
+            identifier = str(i)
+            break
     
+    if identifier == '-1':
+        identifier = str(len(urls))
+    # identifier = hashlib.sha256(url.encode()).hexdigest()
+    urls[identifier] = url
+    return identifier, 201
+
 # delete all the identifiers
 @app.route("/", methods=["DELETE"])
 def delete_identifiers():
@@ -65,9 +72,10 @@ def delete_identifiers():
     return "All Deleted", 404
 
 def check_url_validity(url):
-    # regex pattern for url validation
-    pattern = re.compile(r"(http|https)://[a-zA-Z0-9\-.]+\.[a-zA-Z]{2,3}(/\S*)?")
-    if pattern.match(url):
+    # regex pattern for url validation, source from https://uibakery.io/regex-library/url-regex-python
+    pattern_1 = re.compile("^https?:\\/\\/(?:www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b(?:[-a-zA-Z0-9()@:%_\\+.~#?&\\/=]*)$")
+    pattern_2 = re.compile("^[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b(?:[-a-zA-Z0-9()@:%_\\+.~#?&\\/=]*)$")
+    if pattern_1.match(url) or pattern_2.match(url):
         return True
     else:
         return False
