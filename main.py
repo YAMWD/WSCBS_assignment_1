706b95fc31e8c38f8c67e602c58fbd6f5e032e8a
MingjiangMa <m.ma@student.vu.nl>
2024-02-13 21:15:26 +0100

    add comments

--- main.py
+++ main.py
@@ -7,41 +7,44 @@ import json
 urls = {}
 
 app = Flask(__name__)
-
+#Generate hash value of url
 def hash(url):
 
-    ans = 0
+    ans = 0 #hash value
 
-    for letter in url:
+    for letter in url: #sum ASCII value of all letters in url
         ans += ord(letter)
         # ans = ans % limit
     
-    while ans in urls.keys():
+    while ans in urls.keys(): #if there is same ASCII sum , the ans will plus one to make them different
         ans += 1
         # ans = ans % limit
 
     return ans
-
+#shorten the hash. The hash(url) returns an integer, this funtion turn 10 base into 64base
 def int2base64(n):
+    #make a base64 table, list by index-character 
     table = list(string.ascii_uppercase + string.ascii_lowercase + string.digits + '+' + '/')
+    #turn integer into binary
     s = bin(n)[2:]
-    
+    #if the length of binary sequence is not a multiple of 6, to ensure the base conversion, 0 is added in front
     if len(s) % 6 != 0:
         s = '0' * (6 - len(s) % 6) + s
             
-    ans = ''
-    iter = int(len(s) / 6)
+    ans = '' #initial ans for restoring base64 result
+    iter = int(len(s) / 6) #length of Base64 number
     for i in range(iter):
         start = i * 6
         end = (i + 1) * 6
-        s_par = int(s[start:end], base = 2)
-        ans += table[s_par]
+        s_par = int(s[start:end], base = 2) #convert binary in integer as index
+        ans += table[s_par] #add base64 character into ans, located by index of table
     
     return ans
 
 # get the url of the identifier
 @app.route("/<identifier>", methods=["GET"])
 def get_url(identifier):
+    #if id is found in urls, return its url, else return 404 not found
     url = urls.get(identifier)
     if url != None:
         # desired HTTP status code
@@ -65,22 +68,22 @@ def update_item(identifier):
     url = json.loads(data.decode())['url']
     if urls.get(identifier):
         if check_url_validity(url) == False:
-            return "Invalid URL", 400
+            return "Invalid URL", 400 #400 with an error if the update failed (e.g., the URL was invalid)
         else:
             urls[identifier] = url
             print("1")
-            return "Updated", 200
+            return "Updated", 200 #Updates the URL behind the given ID.
     else:   
-        return "Not Found", 404
+        return "Not Found", 404 #404 if the ID does not exist
     
 # delete the identifier
 @app.route("/<identifier>", methods=["DELETE"])
 def delete_identifier(identifier):
     if urls.get(identifier):
         del urls[identifier]
-        return "Deleted", 204
+        return "Deleted", 204 #find the identifier and delete it 
     else:
-        return "Not Found", 404
+        return "Not Found", 404 #identifier not found
     
 # get all the identifiers
 @app.route("/", methods=["GET"])
@@ -95,8 +98,9 @@ def create_identifier():
     # Check URL validity with a regex expression before creating a mapping for it
     if check_url_validity(url) == False:
         return  "Invalid URL", 400
+    # Check whether the url already exists or not
     if url in urls.values():
-        id = [k for k, v in urls.items() if v == url][0]
+        id = [k for k, v in urls.items() if v == url][0] 
         return 'identifier of {} already exists'.format(url), 400
 
     identifier = int2base64(hash(url))
